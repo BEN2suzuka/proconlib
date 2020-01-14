83457b36a8055e0685a2fd890cd3abff0a76d97d
BEN2suzuka <isobe.naoki.suzuka@gmail.com>
2020-01-14 13:04:05 +0900

    add ABC 126 D

---
+++ AtCoder/abc126d.cpp
@@ -0,0 +1,31 @@
+// ABC 126 D
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<pair<int, int>>> G;
+vector<int> ans;
+int INF = 1000000000;
+
+void dfs(int current, int parent, int color) {
+  for (auto nx : G.at(current)) {
+    if (nx.first == parent) continue;
+    if (nx.second % 2 == 0) ans.at(nx.first) = color;
+    else ans.at(nx.first) = (color + 1) % 2;
+    dfs(nx.first, current, ans.at(nx.first));
+  }
+}
+
+int main() {
+  int N; cin >> N;
+  G.resize(N);
+  ans.assign(N, INF);
+  ans.at(0) = 0;
+  for (int i = 0; i < N-1; i++) {
+    int u, v, w; cin >> u >> v >> w;
+    u--; v--;
+    G.at(u).push_back(make_pair(v, w));
+    G.at(v).push_back(make_pair(u, w));
+  }
+  dfs(0, -1, 0);
+  for (int i = 0; i < N; i++) cout << ans.at(i) << endl;
+}
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # 競プロ用ライブラリ
 
+- DFS (深さ優先探索)
+    - [ABC 126 D - Even Relation](https://github.com/BEN2suzuka/proconlib/blob/master/bfs.cpp)
 - BFS (幅優先探索)
     - [ABC 007 C - 幅優先探索](https://github.com/BEN2suzuka/proconlib/blob/master/bfs.cpp)
