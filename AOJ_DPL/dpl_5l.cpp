38622435b76528c82521823430e98b59078f9d43
BEN2suzuka <isobe.naoki.suzuka@gmail.com>
2020-02-05 21:20:29 +0900

    add DPL 5_L

---
+++ AOJ_DPL/dpl_5l.cpp
@@ -0,0 +1,33 @@
+// DPL 5_L
+// C++14
+#include <bits/stdc++.h>
+using namespace std;
+
+// ボール (N 個) : 区別しない
+// 箱 (K 個) : 区別しない
+// 箱の中身は 1 つ以上
+// <=> ボール N-K 個、箱 K 個、入れ方に制限なし
+// -> 分割数 P(N-K, K)
+
+// 分割数 P(N, K) : 自然数 N を K 個の非負整数の和で表すときの場合の数
+// P(N, K) = P(N, K-1) + P(N-K, K)
+
+int main() {
+  int N, K; cin >> N >> K;
+  const int MOD = 1000000007;
+  const int MAX = 1001;
+  vector<vector<long long>> dp(MAX, vector<long long>(MAX));
+  for (int j = 0; j <= N; j++) dp.at(0).at(j) = 1;
+  for (int i = 1; i <= N; i++) {
+    for (int j = 1; j <= K; j++) {
+      if (i - j >= 0) {
+        dp.at(i).at(j) = (dp.at(i).at(j-1) + dp.at(i-j).at(j)) % MOD;
+      }
+      else {
+        dp.at(i).at(j) = dp.at(i).at(j-1);
+      }
+    }
+  }
+  if (N - K < 0) cout << 0 << endl;
+  else cout << dp.at(N-K).at(K) << endl;
+}
