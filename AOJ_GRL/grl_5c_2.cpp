126a76192fdfb350ea9a60d9aaef5d22dc43477e
BEN2suzuka <isobe.naoki.suzuka@gmail.com>
2020-02-19 00:49:41 +0900

    fix GRL 5_C (Euler Tour)

--- AOJ_GRL/grl_5c_2.cpp
+++ AOJ_GRL/grl_5c_2.cpp
@@ -6,26 +6,28 @@ using namespace std;
 // 最小共通祖先 (LCA : Lowest Common Ancestor)
 // 根付き木の 2 頂点 u, v の共通祖先で最も根から遠い祖先
 
-// Euler Tour
 vector<vector<int>> G;
-vector<int> pre;  // pre[i] := 頂点 i を最初に訪問するまでに何歩歩いたか
-vector<int> EulerTour;  // DFS の訪問順をもつ (2*N-1 要素)
-vector<int> A;  // EulerTour をセグ木でもつ
-int num = 1;
 const int INF = 2147483647;  // 2^31 - 1
 
-// Euler Tour
-void dfs(int v) {
-  pre.at(v) = EulerTour.size();
-  EulerTour.push_back(v);
+// Euler Tour (DFS 木も作る)
+// t := タイマー
+// pre[i] := もとの頂点番号 i に対応する DFS 木の頂点番号
+// ET[i] := DFS の訪問順をもつ (2*N-1 要素)
+// step[i] := 頂点 i を最初に訪問するまでに何歩歩いたか
+void dfs(int v, int &t, vector<int> &pre, vector<int> &ET, vector<int> &step) {
+  int preOrder = t;
+  pre.at(v) = preOrder;
+  t++;
+  step.at(pre.at(v)) = ET.size();
+  ET.push_back(pre.at(v));
   for (auto nx : G.at(v)) {
-    dfs(nx);
-    EulerTour.push_back(v);
+    dfs(nx, t, pre, ET, step);
+    ET.push_back(pre.at(v));
   }
 }
 
 // A.at(i) を x に更新
-void update(int i, int x) {
+void update(vector<int> &A, int i, int x, int num) {
   i += num - 1;  // ノード番号
   A.at(i) = x;
   while (i > 0) {
@@ -36,12 +38,12 @@ void update(int i, int x) {
 
 // [a, b) の最小値を求める
 // ノード番号 k は [l, r) に対応している
-int query(int a, int b, int k, int l, int r) {
+int query(vector<int> &A, int a, int b, int k, int l, int r) {
   if (r <= a || b <= l) return INF;  // [a, b) と [l, r) が交差していない
   if (a <= l && r <= b) return A.at(k);  // [a, b) が [l, r) を完全に含む
   else {
-    int c1 = query(a, b, 2*k+1, l, (l+r)/2);
-    int c2 = query(a, b, 2*k+2, (l+r)/2, r);
+    int c1 = query(A, a, b, 2*k+1, l, (l+r)/2);
+    int c2 = query(A, a, b, 2*k+2, (l+r)/2, r);
     return min(c1, c2);
   }
 }
@@ -49,7 +51,7 @@ int query(int a, int b, int k, int l, int r) {
 int main() {
   int N; cin >> N;
   G.resize(N);
-  pre.resize(N);
+  vector<int> pre(N), ET, step(N);
   for (int i = 0; i < N; i++) {
     int k; cin >> k;
     for (int j = 0; j < k; j++) {
@@ -57,16 +59,23 @@ int main() {
       G.at(i).push_back(c);
     }
   }
-  dfs(0);
+  int timer = 0;
+  dfs(0, timer, pre, ET, step);
+  vector<int> rev(N);  // DFS 木の頂点番号 -> もとの頂点番号
+  for (int i = 0; i < N; i++) rev.at(pre.at(i)) = i;
+  int num = 1;
   while (num < 2*N-1) num *= 2;
-  A = vector<int>(2*num - 1, 0);
-  for (int i = 0; i < 2*N-1; i++) update(i, EulerTour.at(i));
+  vector<int> A(2*num - 1, 0);  // EulerTour をセグ木で管理
+  for (int i = 0; i < 2*N-1; i++) update(A, i, ET.at(i), num);
   int Q; cin >> Q;
   for (int i = 0; i < Q; i++) {
     int u, v; cin >> u >> v;
     if (u == v) { cout << u << endl; continue; }
-    if (pre.at(u) > pre.at(v)) swap(u, v);
-    // LCA(i, j) は RMQ[ pre[i], pre[j] ) (i < j)
-    cout << query(pre.at(u), pre.at(v), 0, 0, num) << endl;
+    u = pre.at(u);  // DFS 木の頂点番号に変換
+    v = pre.at(v);
+    if (step.at(u) > step.at(v)) swap(u, v);
+    // LCA(i, j) は RMQ[ step[i], step[j] ) (DFS 木の頂点 i < j)
+    int c = query(A, step.at(u), step.at(v), 0, 0, num);
+    cout << rev.at(c) << endl;
   }
 }
